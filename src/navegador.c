57a9210214377a2ff60e59419214ebce2cc7d9c6
CalexODE <33105149+CalexODE@users.noreply.github.com>
2018-11-05 09:39:26 -0300

    Add files via upload

--- src/navegador.c
+++ src/navegador.c
@@ -1,14 +1,53 @@
 #include <stdio.h>
+#include "database.h"
 int main(){	
-	printf("%s\n","Digite 1 para criar uma tabela:");
-	printf("%s\n","Digite 2 para listar todas as tabelas:");
-	printf("%s\n","Digite 3 para criar uma nova linha na tabela:");	
-	printf("%s\n","Digite 4 para listar todos os dados de uma tabela:");
-	printf("%s\n","Digite 5 Pesquisar valor em uma tabela:");
-	printf("%s\n","Digite 6 para apagar valor de uma tabela:");
-	printf("%s\n","Digite 7 para apagar uma tabela:");
 	int opcao;
-	scanf("%d",&opcao);
-	
+	do{
+		printf("%s\n","Digite 1 para criar uma tabela:");
+		printf("%s\n","Digite 2 para listar todas as tabelas:");
+		printf("%s\n","Digite 3 para criar uma nova linha na tabela:");	
+		printf("%s\n","Digite 4 para listar todos os dados de uma tabela:");
+		printf("%s\n","Digite 5 Pesquisar valor em uma tabela:");
+		printf("%s\n","Digite 6 para apagar valor de uma tabela:");
+		printf("%s\n","Digite 7 para apagar uma tabela:");
+		
+		scanf("%d",&opcao);
+
+		switch(opcao)
+		{
+			case 0:
+				break;
+			case 1:
+
+				break;
+
+			case 2:
+
+				break;
+
+			case 3:
+
+				break;
+
+			case 4:
+
+				break;
+
+			case 5:
+
+				break;
+
+			case 6:
+
+				break;
+
+			case 7:
+
+				break;
+
+			default:
+				printf("Valor Invalido\n");
+		}	
+	}while(opcao != 0);
 	return 0;
 }
