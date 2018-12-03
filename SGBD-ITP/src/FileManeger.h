4786f72085d33f130d55e88cff1538d67ba16876
Alexandre Alves Andrade <33105149+CalexODE@users.noreply.github.com>
2018-12-03 10:17:08 -0300

    Add files via upload

---
+++ SGBD-ITP/src/FileManeger.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct{
+	char nome[40];
+	int tipo;
+	char valor[200];
+}colunaNomeTipo;
+
+typedef struct{
+	char valor[200];
+}valoresPrimarios;
+
+typedef struct{
+	char valor[40];
+}variaveis;
+
+variaveis **valor;
+
+int criarTabela(char* tabela);
+
+int criarLinha(char* tabela);
+
+int apagarValor(char* tabela);
+
+int apagarTabela(char* tabela);
